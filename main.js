1ca4d186a0847e3b7beb5d92f03940456851f021
NehaDevs <99113186+NehaDevs@users.noreply.github.com>
2022-06-10 12:01:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('Hello World!');
+
+
+var date = document.getElementById("dateinput");
+
+
+function check(){
+  if(date.value == "2022-05-06"){
+    window.location = 'owo.html';
+  }
+}
